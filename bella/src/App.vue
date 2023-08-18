67de2cde26f43f0a91583569c1ea2c203b8fea12
RidhaaGabier <2811rg@gmail.com>
2023-08-18 15:13:17 +0200

    first commit

--- bella/src/App.vue
+++ bella/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <NavBarComp/>
   <router-view/>
-</template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+   <FooterComp/>
+ </template>
+  
+ 
+ 
+ <script>
+ 
+ import NavBarComp from '@/components/NavBarComp.vue'
+ import FooterComp from '@/components/FooterComp.vue'
+ export default{
+   components:{NavBarComp,FooterComp}
+ }
+ </script>
+ 
+ <style>
+ #app {
+   font-family: Avenir, Helvetica, Arial, sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   text-align: center;
+   color: #2c3e50;
+ }
+ 
+ nav {
+   padding: 30px;
+ }
+ 
+ nav a {
+   font-weight: bold;
+   color: #2c3e50;
+ }
+ 
+ nav a.router-link-exact-active {
+   color: #42b983;
+ }
+ </style>
+ 
