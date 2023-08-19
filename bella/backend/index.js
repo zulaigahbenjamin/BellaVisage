c489dc49fc1d00090a6eee3243260d179cb7fc6b
Zulaigah <zulaigahbenjamin01@gmail.com>
2023-08-19 12:35:15 +0200

    api

--- bella/backend/index.js
+++ bella/backend/index.js
@@ -3,14 +3,19 @@ import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 import Router from "./routes/route.js";
+const allowedOrigins = ['http://localhost:8080', 'http://localhost:8081'];
 
 // Assuming your './config/database.js' exports the 'connection' object
 import connection from './config/database.js';
 
 const app = express();
+const corsOptions = {
+    origin: allowedOrigins
+  };
 const port = +process.env.PORT || 5002
 
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,7 +25,13 @@ app.get('/', (req, res)=> {
     res.json({
         msg: "You're home"
     })
-})
+});
+app.post('/products', (req, res) => {
+    const newProduct = req.body;
+    products.push(newProduct);
+    res.status(201).json(newProduct);
+  });
+
 
     // Start the server after connecting to the database
     app.listen(5002, () =>
--- bella/backend/models/productModels.js
+++ bella/backend/models/productModels.js
@@ -4,7 +4,7 @@ import db from "../config/database.js";
 // Get All Products
 export const getProducts = (result) => {
     db.query("SELECT * FROM products;", (err, results) => {             
-        if(err) {
+        if (err) {
             console.log(err);
             result(err, null);
         } else {
@@ -13,50 +13,65 @@ export const getProducts = (result) => {
     });   
 }
   
-// Get Single Product
+// Get Single Product by ID
 export const getProductById = (id, result) => {
-    db.query("SELECT * FROM product WHERE product_id = ?", [id], (err, results) => {             
-        if(err) {
+    db.query("SELECT * FROM products WHERE id = ?", [id], (err, results) => {             
+        if (err) {
             console.log(err);
             result(err, null);
         } else {
-            result(null, results[0]);
+            if (results.length === 0) {
+                result({ message: "Product not found" }, null);
+            } else {
+                result(null, results[0]);
+            }
         }
     });   
 }
   
 // Insert Product to Database
-export const insertProduct = (data, result) => {
-    db.query("INSERT INTO product SET ?", [data], (err, results) => {             
-        if(err) {
-            console.log(err);
-            result(err, null);
-        } else {
-            result(null, results);
-        }
-    });   
+export const insertProduct = async (data) => {
+    return new Promise((resolve, reject) => {
+        db.query("INSERT INTO products SET ?", [data], (err, results) => {             
+            if (err) {
+                console.log(err);
+                reject(err);
+            } else {
+                resolve(results);
+            }
+        });   
+    });
 }
+
   
-// Update Product to Database
+// Update Product in Database by ID
 export const updateProductById = (data, id, result) => {
-    db.query("UPDATE product SET product_name = ?, product_price = ? WHERE product_id = ?", [data.product_name, data.product_price, id], (err, results) => {             
-        if(err) {
+    db.query("UPDATE products SET name = ?, price = ? WHERE id = ?", [data.name, data.price, id], (err, results) => {             
+        if (err) {
             console.log(err);
             result(err, null);
         } else {
-            result(null, results);
+            if (results.affectedRows === 0) {
+                result({ message: "Product not found" }, null);
+            } else {
+                result(null, results);
+            }
         }
     });   
 }
   
-// Delete Product to Database
+// Delete Product from Database by ID
 export const deleteProductById = (id, result) => {
-    db.query("DELETE FROM product WHERE product_id = ?", [id], (err, results) => {             
-        if(err) {
+    db.query("DELETE FROM products WHERE id = ?", [id], (err, results) => {             
+        if (err) {
             console.log(err);
             result(err, null);
         } else {
-            result(null, results);
+            if (results.affectedRows === 0) {
+                result({ message: "Product not found" }, null);
+            } else {
+                result(null, results);
+            }
         }
     });   
 }
