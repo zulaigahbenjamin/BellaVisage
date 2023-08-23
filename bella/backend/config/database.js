fe5004dbd4b3b355128527891cdc8c7bdb9986b1
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-08-23 11:20:49 +0200

    Config - Updates

--- bella/backend/config/database.js
+++ bella/backend/config/database.js
@@ -2,7 +2,6 @@ import mysql2 from "mysql2";
 import dotenv from 'dotenv';
 
 dotenv.config();
- 
 // create the connection to database
 const db = mysql2.createPool({
   host     : process.env.host,
@@ -12,35 +11,5 @@ const db = mysql2.createPool({
   multipleStatements: true,
   connectionLimit:30,
 });
-
-db.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    db.query("select * from products", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-  });
-
-
-  // db.query("INSERT INTO todos SET ? ", params,
-  // function (error, results, fields) {
-  //     if (error) throw error;
-  //     console.log("Record inserted");
-  // });
-
-
-  // db.connect(function(err) {
-  //   if (err) throw err;
-  //   console.log("Connected!");
-  //   var sql = "INSERT INTO products id, image, size,description, price VALUES ?";
-   
-  //   db.query(sql, function (err, result) {
-  //     if (err) throw err;
-  //     console.log("1 record inserted");
-  //   });
-  // });
-;
-
 
 export default db;
