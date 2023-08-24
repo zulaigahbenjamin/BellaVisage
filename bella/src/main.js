ec93ee3e61c89cce4896e2f518f273a72bd89a69
zulaigahbenjamin <130163047+zulaigahbenjamin@users.noreply.github.com>
2023-08-24 12:54:46 +0200

    Completed backend

--- bella/src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-// import cors from 'cors'
-import AddProduct from './components/AddProduct.vue'
-
-createApp(App).use(store).use(AddProduct).use(router).mount('#app')
